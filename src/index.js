54a992f793507631bb9d5c845863cfb1c7a2acc2
hazrat120 <125098771+hazrat120@users.noreply.github.com>
2024-01-20 23:11:06 +0600

    My first react code

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+// export default function Profile() {
+//   return <img src="https://i.imgur.com/MK3eW3Am.jpg" alt="Katherine Johnson" />;
+// }
+
+const element = React.createElement("p", null, "Hazrat Ali");
+
+console.dir(element);
